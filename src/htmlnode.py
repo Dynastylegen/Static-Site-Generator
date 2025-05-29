afb8157dff73dd34a5924ea9508f023ca3b09905
Dynastylegen <peter.madden.dynasty@gmail.com>
2025-05-29 22:59:42 +0100

    Added LeafNode Child class to htmlnode.py file, added LeafNode class tests to test_htmlnode.py file.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,3 +15,17 @@ class HTMLNode:
 
     def __repr__(self):
         return (f"HTMLNode: {self.tag}, {self.value}, {self.children}, {self.props}")
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(value=value, tag=tag, props=props, children=None)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        props_str = ""
+        if self.props:
+            props_str = " " + " ".join(f'{key}="{value}"' for key, value in self.props.items())
+        return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_none(self):
@@ -14,6 +14,19 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_single(self):
         node = HTMLNode(props={"id": "unique"})
         self.assertEqual(node.props_to_html(), ' id="unique"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("Hello, world!", "p")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode("Raw text", None)
+        self.assertEqual(node.to_html(), "Raw text")
+
+    def test_leaf_to_html_value_error(self):
+        node = LeafNode(None, "p")
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 if __name__ == "__main__":
     unittest.main()
