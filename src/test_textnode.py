d69bcf719edb4d64a08d81f0362d644e4f86e805
Dynastylegen <peter.madden.dynasty@gmail.com>
2025-06-02 19:09:47 +0100

    Added Split Delimiter function to textnode.py file, added relevent tests to test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -24,6 +24,67 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("A", TextType.ITALIC)
         self.assertNotEqual(node, node2, msg="Nodes with different texttype should not be equal")
 
+    def test_delimiters_work(self):
+        node = TextNode("some `code` here", TextType.TEXT)
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("some ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" here", TextType.TEXT)
+        ]
+        self.assertEqual(result, expected)
+
+    def test_no_delimiters(self):
+        node = TextNode("some text here", TextType.TEXT)
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [node]
+        self.assertEqual(result, expected)
+
+    def test_unclosed_delimiter(self):
+        node = TextNode("some `code here", TextType.TEXT)
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_multiple_delimiters(self):
+        node = TextNode("some 'code' *bold*", TextType.TEXT)
+        first_pass = split_nodes_delimiter([node], "'", TextType.CODE)
+        second_pass = split_nodes_delimiter(first_pass, "*", TextType.BOLD)
+        result = second_pass
+        expected = [
+            TextNode("some ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD)
+        ]
+        self.assertEqual(result, expected)
+
+    def test_delimiters_at_boundary(self):
+        node = TextNode("`atstart` and end", TextType.TEXT)
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("atstart", TextType.CODE),
+            TextNode(" and end", TextType.TEXT)
+        ]
+        self.assertEqual(result, expected)
+
+    def test_delimiters_at_boundary_end(self):
+        node = TextNode("atstart and `end`", TextType.TEXT)
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("atstart and ", TextType.TEXT),
+            TextNode("end", TextType.CODE)
+        ]
+        self.assertEqual(result, expected)
+
+    def test_empty_text_between_delimiters(self):
+        node = TextNode("start `` end", TextType.TEXT)
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+            TextNode("start ", TextType.TEXT),
+            TextNode("", TextType.CODE),
+            TextNode(" end", TextType.TEXT)
+        ]
+        self.assertEqual(result, expected)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -21,3 +21,28 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for node in old_nodes:
+
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        split_text = node.text.split(delimiter)
+
+        if len(split_text) == 1:
+            new_nodes.append(node)
+            continue
+
+        if len(split_text) % 2 == 0:
+            raise Exception("Unclosed delimiter detected")
+
+        for i, text in enumerate(split_text):
+            node_type = text_type if i % 2 else TextType.TEXT
+            if text != "" or i % 2 == 1:
+                new_nodes.append(TextNode(text, node_type))
+
+    return new_nodes
